e755c9e18921cdce06ee0aad1dae937d1de7ea0c
MD-Kayesur <rmdkayesur@gmail.com>
2026-06-10 16:13:37 +0600

    implement Media module with controller and service layer

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './module/auth/auth.module';
+import { MediaModule } from './module/media/media.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, MediaModule],
   controllers: [AppController],
   providers: [AppService],
 })
